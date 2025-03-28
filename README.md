7d086b9e9dc630e6d2a9cc3db70c49fb9e9991ee
ghepwort <ghepwort@uwyo.edu>
2025-03-28 07:06:44 -0600

    It's weird how despite me using an extra for loop in my hasCycle program, it is asymptotically the same as my depthFirstSearch.

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ the function and run automatically when you commit through a GitHub action.
 
 What is the worst-case big $\Theta$ complexity of your implementation? Add your
 answer, including your reasoning, to this markdown file.
+
+## Answers
+
+The worst-case big $\Theta$ complexity is $\Theta(|V| \cdot |E|)$. First we initilize the the visited and the stack which both take $|V|$. Next we iterate through every node and run depthFirstSearch. The loop will take $V$ time and the depthFirstSearch in the worst case will run through every node and edge which will give us $|V|+|E|$. All together this gives us a time complexity of $|V|+|V|+|V| \cdot (|V|+|E|)= 2|V|+|V|^2+|V| \cdot |E|$. In the worst cast $|E| = |V|^2$ so asymptoticaly this means that the overall time complexity in the worst case is $\Theta(|V| \cdot |E|)$.
+
+## Sources
+
+Not really a "source", but [this](https://csacademy.com/app/graph_editor/) website helped me a lot with drawing the graphs.
+
+"I certify that I have listed all sources used to complete this exercise, including the use of any Large Language Models. All of the work is my own, except where stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is suspected, charges may be filed against me without prior notice." 
--- code.js
+++ code.js
@@ -1,3 +1,47 @@
 function hasCycle(graph) {
+    function depthFirstSearch(currentNode, visited, stack) {
+        if (!visited[currentNode]) {
+            visited[currentNode] = true;
+            stack[currentNode] = true;
+
+            for (let neighbor of graph[currentNode] || []) {
+                if (!visited[neighbor] && depthFirstSearch(neighbor, visited, stack)) {
+                    return true;
+                } else if (stack[neighbor]) {
+                    return true; // Cycle detected
+                }
+            }
+        }
+
+        stack[currentNode] = false; // Remove from recursion stack
+        return false;
+    }
+
+
+    visited = []
+    for (i = 0; i < graph.length; i++){
+        visited[i] = false;
+    }
+
+    stack = []
+    for (i = 0; i < graph.length; i++){
+        stack[i] = false;
+    }
+
+
+    // Loop through every node and run DFS
+    // This DFS runs very similar to graph search, but now it only takes two inputs, the graph and the target node
+    // It runs the same as DFS and looks to see if it can go from the start back to itself and if it can theres a cycle, otherwise it goes to the next node and tries the same thing
+    for (let i = 0; i < graph.length; i++) {
+        if (!visited[i]) {
+            if (depthFirstSearch(i, visited, stack)) {
+                return true;
+            }
+        }
+    }
+
     return false;
 }
+
+
+
